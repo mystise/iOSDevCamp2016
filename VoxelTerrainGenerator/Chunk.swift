26c8f4ed0c215848b559aac46574a4d1bc8972c6
mystise <246232+mystise@users.noreply.github.com>
2016-07-22 19:14:50 -0700

    Initial commit

---
+++ VoxelTerrainGenerator/Chunk.swift
@@ -0,0 +1,18 @@
+//
+//  Chunk.swift
+//  VoxelTerrainGenerator
+//
+//  Created by Adalynn Dudney on 7/22/16.
+//  Copyright © 2016 mystise. All rights reserved.
+//
+
+import Foundation
+
+public enum Block: UInt8 {
+    case Air
+    case Stone
+    case Dirt
+    case Grass
+    case Wood
+    case Leaf
+}
